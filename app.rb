7d48f09c8bbc90796b18f31c47b03d5b7ae59db3
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-10-26 16:41:51 +0000

    square calculator works

--- app.rb
+++ app.rb
@@ -8,13 +8,29 @@ get("/") do
   "
 end
 get("/square/new") do
-  @num = params.fetch("input").to_i
-erb(:square_new)
+  erb(:square_new)
 end
+get("/square/results") do
+  @num = params.fetch("num").to_i
+  @square = @num ** 2
+  erb(:square_new)
+end
+
 get("/square_root/new") do
   erb(:square_root_new)
 end
+
+get("/square_root/results") do
+  @num = params.fetch("num").to_i
+  @sqrt = @num ** .5
+  erb(:square_root_new)
+end
 get("/random/new") do
+  erb(:random_new)
+end
+get("/random/results") do
+  @num = params.fetch("num").to_i
+  @square = @num ** 2
   erb(:random_new)
 end
 get("/payment/new") do
