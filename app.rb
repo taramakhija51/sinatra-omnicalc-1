7035df0df179b315491e0ee395fb66c218e14b10
taramakhija51 <114837329+taramakhija51@users.noreply.github.com>
2024-10-26 12:54:07 +0000

    routes

--- app.rb
+++ app.rb
@@ -7,3 +7,16 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+get("/square/new") do
+  @num = params.fetch("input").to_i
+erb(:square_new)
+end
+get("/square_root/new") do
+  erb(:square_root_new)
+end
+get("/random/new") do
+  erb(:random_new)
+end
+get("/payment/new") do
+  erb(:payment_new)
+end
